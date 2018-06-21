270d0dd8b4680be3eb918258c043da78c82365ea
PierreBaoy <Baoy@Libaoyu.lan>
2018-06-22 00:02:28 +0800

    init

---
+++ BoxPassword/BoxPassword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoxPassword
+//
+//  Created by PierreBaoy on 2018/6/22.
+//  Copyright © 2018年 PierreBaoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
